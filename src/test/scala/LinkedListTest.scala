e01adb6ffa81c2ed4d99887ddae438a7bdc9483a
Mitchell Lisle <mitchelllisle@mailbox.org>
2023-11-20 22:20:31 +1100

    add tests

--- src/test/scala/LinkedListTest.scala
+++ src/test/scala/LinkedListTest.scala
@@ -3,11 +3,20 @@ package io.github.mitchelllisle
 import org.scalatest.flatspec.AnyFlatSpec
 
 class LinkedListTest extends AnyFlatSpec {
-  "adding a value" should "have one value" in {
+  "adding a node" should "make a linked list with one node" in {
     val list = new LinkedList[Int]
     list.insertAtFront(1)
 
     val nodes = list.collect()
     assert(nodes.length == 1)
+  }
+
+  "deleting a node" should "make a linked list with no nodes" in {
+    val list = new LinkedList[Int]
+    list.insertAtFront(1)
+
+    list.deleteValue(1)
+    val nodes = list.collect()
+    assert(nodes.isEmpty)
   }
 }
