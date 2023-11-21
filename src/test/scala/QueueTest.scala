a4b26214e53593901296c14d75c4555f996df803
Mitchell Lisle <mitchelllisle@mailbox.org>
2023-11-21 21:08:03 +1100

    add queue

--- src/test/scala/QueueTest.scala
+++ src/test/scala/QueueTest.scala
@@ -3,7 +3,11 @@ package io.github.mitchelllisle
 import org.scalatest.flatspec.AnyFlatSpec
 
 class QueueTest extends AnyFlatSpec {
-  "" should "" in {
-    true
+  "Peeking" should "return the head" in {
+    val q = new Queue[Int]
+
+    q.enqueue(10)
+    assert(q.peek().get == 10)
+
   }
 }
